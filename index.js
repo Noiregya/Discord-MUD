0a8e6d29492cc643a3cd7be8f707df5c5e750464
Noiregya <noiregya@gmail.com>
2019-06-24 19:37:44 +0200

    Bot can launch

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+//Includes
 const Discord = require('discord.js')
-const token = ''
+const yaml = require('js-yaml')
+const fs = require('fs');
+//Objects
+const client = new Discord.Client()
+try {
+  var config = yaml.safeLoad(fs.readFileSync(__dirname+'/secret.yaml', 'utf8'));
+} catch (e) {
+  console.log(e);
+}
+
+//Consts
 const prefix = '.M'
+const token = config['token']
 
 //Client initialisation
 client.on('ready', function(){
-  console.log('Welcome to pin_archiver.'+'\n')
+  console.log('Welcome to MUDBot.'+'\n')
   console.log('Logged in as '+client.user.tag+'!')
   client.user.setActivity(prefix+" HELP",{ type: 'LISTENING' }).then(function(presence){
   },function(err){
     console.log(err)
   })
-}
+})
 
 client.login(token)
