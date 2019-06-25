2f553c1841d532372759357fbfc10d36b0ebf85d
Noiregya <noiregya@gmail.com>
2019-06-26 00:09:05 +0200

    Basic documentation and correction of some classes

--- Player.js
+++ Player.js
@@ -10,10 +10,12 @@ class Player {
    * Creates a Player
    * @param {!Name} name - Name of the Player
    */
-  constructor (name, inventory) {
+  constructor (id, name) {
+    this.id = id
     this.name = name
     this.inventory = new Inventory()
     this.activeQuests = new Array()
+    this.passes = new Array()
   }
 }
 module.exports = Player
--- index.js
+++ index.js
@@ -17,6 +17,32 @@ try {
 //Consts
 const prefix = '.M'
 const token = config['token']
+const commands = {
+  regHelp: /^HELP$|^H$|^MANUAL$|^MAN$/,
+  regBuy: /^PURCHASE$|^BUY$/,
+  regSell: /^SELL$/,
+  regGo: /^GO$|^ENTER$|LEAVE TO$/,
+  regFlee: /^FLEE$|^RUN$/,
+  regLook: /^LOOK$|^WHERE$/,
+  regTake: /^FETCH$|^TAKE$|^GET$/,
+  regGive: /^LEND$|^GIVE$/,
+  regDrop: /^THROW*$|^DROP$|^LEAVE$/,
+  regKill: /^ATTACK$|^KILL$/,
+  regSleep: /^REST$|^SLEEP$/,
+  regKeep: /^KEEP$|^SAVE$/,
+  regBag: /^INVENTORY$|^BAG$/,
+  regLaugh: /^LAUGH$|^GIGGLE$/,
+  regTickle: /^TICKLE$/,
+  regHug: /^HUG$|^CUDDLE$/,
+  regTell: /^SPEAK$|^SAY$|^TELL$/,
+  regKiss: /^KISS$/,
+  regYell: /^SHOUT$|^YELL$|^SCREAM$/,
+  regAct: /^IMITATE|^ACT LIKE$|^ACT/
+}
+const helpMessage = 'You can look around by typing look, open your bag with bag and ask for help. Maybe you can also do other things, who knows?'
+
+//Global vars
+var Players = new Array()
 
 function parseMessage (string) {
   var spaces = string.trim().split('"')
@@ -31,6 +57,14 @@ function parseMessage (string) {
     peer = !peer
   })
   return parsedCommand.filter(value => Object.keys(value).length !== 0)
+}
+
+function sendHelp(author){
+  author.createDM().then(
+    DMChannel =>{
+      DMChannel.send(helpMessage).catch(
+        err => console.log('Could not send DM\n'+err))
+      }, err => console.log('Could not send DM\n'+err))
 }
 
 /* EVENTS */
@@ -50,7 +84,7 @@ client.on('error', function (error) {
 client.on('ready', function(){
   console.log('Welcome to MUDBot.'+'\n')
   console.log('Logged in as '+client.user.tag+'!')
-  client.user.setActivity(prefix+" HELP",{ type: 'LISTENING' }).then(function(presence){
+  client.user.setActivity("Ask for help!",{ type: 'LISTENING' }).then(function(presence){
   },function(err){
     console.log(err)
   })
@@ -61,11 +95,17 @@ client.on('message', function (message) {
   //is the message from the bot itself?
   if (message.author.id === client.user.id) { return }
   else{
+    if (parsedMessage[0].toUpperCase().match(commands.regHelp)) {
+      sendHelp(message.author)
+    }
+    /*
+    DM/Text channel check, might have to be used in command specific conditions
+    instead of outside like here.
     if (message.channel.type === 'text') {
 
     } else if (message.channel.type === 'dm') {
 
-    }
+    }*/
   }
 })
 
